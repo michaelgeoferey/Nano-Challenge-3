cbed5b3bf2f7a79bbc07b4b5dca0904bab09e9e4
Michael Geoferey <57284326+michaelgeoferey@users.noreply.github.com>
2020-06-18 16:16:59 +0700

    test

---
+++ Nano Challenge 3/Nano Challenge 3/Main Menu + Onboard/Onboard.swift
@@ -0,0 +1,37 @@
+//
+//  Onboard.swift
+//  Nano Challenge 3
+//
+//  Created by Michael Geoferey on 12/06/20.
+//  Copyright © 2020 Michael Geoferey. All rights reserved.
+//
+
+import SpriteKit
+
+class Onboard: SKScene{
+
+    var pressStart = SKSpriteNode()
+    let backgroundMusic = SKAudioNode(fileNamed: "onboardBGM.wav")
+    
+    override func didMove(to view: SKView) {
+        pressStart = self.childNode(withName: "PressStart") as! SKSpriteNode
+        self.addChild(backgroundMusic)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           
+           for touch: AnyObject in touches{
+               let pointOfTouch = touch.location(in: self)
+               let skView = self.view as SKView?
+               
+               if pressStart.contains(pointOfTouch) {
+                   
+                   let sceneMoveTo = Splash1(fileNamed: "Splash1Scene")
+                   sceneMoveTo?.scaleMode = self.scaleMode
+                   let sceneTransition = SKTransition.fade(withDuration: 1)
+                   skView?.presentScene(sceneMoveTo!, transition: sceneTransition)
+               }
+           }
+       }
+
+}
